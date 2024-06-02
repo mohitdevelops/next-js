a1334160587969f55bbafba1f8de59686952208f
Mohit <mohit@rohininfo.com>
2024-06-02 14:05:40 +0530

    removed content for main branch

--- app/meals/share/page.js
+++
@@ -1,5 +0,0 @@
-export default function Share(){
-    return <main>
-        <h1>Share</h1>
-    </main>
-}
--- app/not-found.js
+++
@@ -1,6 +0,0 @@
-export default function NotFound(){
-    return <main className="not-found">
-        <h1>Meal not found</h1>
-        <p>Meal you are looking for is not found.</p>
-    </main>
-}
